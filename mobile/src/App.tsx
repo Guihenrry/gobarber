64a5eab87efcc265da250c6909b57e78fa883a83
Gui <guilhermehenry60@gmail.com>
2020-05-06 21:36:04 -0300

    feat(mobile): create project react native with typescript template

---
+++ mobile/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, StatusBar } from 'react-native';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#312E38" />
+      <View style={{ backgroundColor: '#312E38', flex: 1 }} />
+    </>
+  );
+};
+
+export default App;
