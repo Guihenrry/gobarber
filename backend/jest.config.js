58a80aa677c83559ba8eec6684b24a2f4c67f9a4
Gui <guilhermehenry60@gmail.com>
2020-05-13 18:50:37 -0300

    feat(backend): add env variables

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -118,7 +118,7 @@ module.exports = {
   // runner: "jest-runner",
 
   // The paths to modules that run some code to configure or set up the testing environment before each test
-  setupFiles: ["./jest-setup-file.ts"],
+  setupFiles: ["<rootDir>/jest-setup-file.ts"],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
   // setupFilesAfterEnv: [],
