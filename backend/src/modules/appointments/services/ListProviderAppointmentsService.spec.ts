b5336bfb4f95a78bdfb71a73f0bd9ef28108299c
Gui <guilhermehenry60@gmail.com>
2020-05-14 18:08:57 -0300

    feat(backend): add cache in list providers and appoitments in day

--- backend/src/modules/appointments/services/ListProviderAppointmentsService.spec.ts
+++ backend/src/modules/appointments/services/ListProviderAppointmentsService.spec.ts
@@ -1,14 +1,19 @@
+import FakeCacheProvider from '@shared/container/providers/CacheProvider/fakes/FakeCacheProvider';
 import ListProviderAppointmentService from './ListProviderAppointmentsService';
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 
 let listProviderAppointment: ListProviderAppointmentService;
+let fakeCacheProvider: FakeCacheProvider;
 let fakeAppointmentsRepository: FakeAppointmentsRepository;
 
 describe('ListProviderAppointment', () => {
   beforeEach(() => {
     fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    fakeCacheProvider = new FakeCacheProvider();
+
     listProviderAppointment = new ListProviderAppointmentService(
       fakeAppointmentsRepository,
+      fakeCacheProvider,
     );
   });
 
@@ -33,5 +38,40 @@ describe('ListProviderAppointment', () => {
     });
 
     expect(appointments).toEqual([appointment1, appointment2]);
+  });
+
+  it('should be able cache list the provider appointments in on day', async () => {
+    const appointment1 = await fakeAppointmentsRepository.create({
+      date: new Date(2020, 4, 12, 12),
+      provider_id: 'provider_id',
+      user_id: 'user_id',
+    });
+
+    const appointment2 = await fakeAppointmentsRepository.create({
+      date: new Date(2020, 4, 12, 13),
+      provider_id: 'provider_id',
+      user_id: 'user_id',
+    });
+
+    const findAllInDayFromProvider = jest.spyOn(
+      fakeAppointmentsRepository,
+      'findAllInDayFromProvider',
+    );
+
+    await listProviderAppointment.execute({
+      provider_id: 'provider_id',
+      day: 12,
+      month: 5,
+      year: 2020,
+    });
+
+    await listProviderAppointment.execute({
+      provider_id: 'provider_id',
+      day: 12,
+      month: 5,
+      year: 2020,
+    });
+
+    expect(findAllInDayFromProvider).toHaveBeenCalledTimes(1);
   });
 });
--- backend/src/modules/appointments/services/ListProvidersService.spec.ts
+++ backend/src/modules/appointments/services/ListProvidersService.spec.ts
@@ -1,14 +1,20 @@
+import FakeCacheProvider from '@shared/container/providers/CacheProvider/fakes/FakeCacheProvider';
 import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
 import ListProvidersService from './ListProvidersService';
 
 let fakeUsersRepository: FakeUsersRepository;
+let fakeCacheProvider: FakeCacheProvider;
 let listProviders: ListProvidersService;
 
 describe('ListProviders', () => {
   beforeEach(() => {
     fakeUsersRepository = new FakeUsersRepository();
+    fakeCacheProvider = new FakeCacheProvider();
 
-    listProviders = new ListProvidersService(fakeUsersRepository);
+    listProviders = new ListProvidersService(
+      fakeUsersRepository,
+      fakeCacheProvider,
+    );
   });
 
   it('should be able to list the providers', async () => {
@@ -35,5 +41,35 @@ describe('ListProviders', () => {
     });
 
     expect(users).toEqual([user1, user2]);
+  });
+
+  it('should be able cache the list the providers', async () => {
+    const user1 = await fakeUsersRepository.create({
+      name: 'John Due',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    const loggedUser = await fakeUsersRepository.create({
+      name: 'GoBarber',
+      email: 'admin@gobarber.com',
+      password: '123456',
+    });
+
+    const findAllProviders = jest.spyOn(
+      fakeUsersRepository,
+      'findAllProviders',
+    );
+
+    await listProviders.execute({
+      user_id: loggedUser.id,
+    });
+
+    const users = await listProviders.execute({
+      user_id: loggedUser.id,
+    });
+
+    expect(users).toEqual([user1]);
+    expect(findAllProviders).toHaveBeenCalledTimes(1);
   });
 });
